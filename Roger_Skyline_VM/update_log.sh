6b6b01001df28959edec380f11a52a8c995cdfe3
Terry FOURGEUX <tefourge@e2r12p23.42.fr>
2019-07-15 16:04:08 +0200

    Update script for logs

--- Roger_Skyline_VM/update_log.sh
+++ Roger_Skyline_VM/update_log.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update >> /var/log/update_script.log && apt full-upgrade >> /var/log/update_script.log 2>&1
+apt update >> /var/log/update_script.log 2>&1 && apt -y full-upgrade >> /var/log/update_script.log 2>&1
